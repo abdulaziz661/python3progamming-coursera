96f5029a2a18fffaae95b08ed1d0282ff506105e
abdulaziz661 <ansariabdulaziz65@gmail.com>
2019-11-06 16:14:56 +0530

    yes

--- course_1_assessment_7.py
+++ course_1_assessment_7.py
@@ -3,70 +3,70 @@
 #Hard-coded answers will receive no credit.
 
 
-# rainfall_mi = "1.65, 1.46, 2.05, 3.03, 3.35, 3.46, 2.83, 3.23, 3.5, 2.52, 2.8, 1.85"
+rainfall_mi = "1.65, 1.46, 2.05, 3.03, 3.35, 3.46, 2.83, 3.23, 3.5, 2.52, 2.8, 1.85"
 
-# num_rainy_months = []
+num_rainy_months = []
 
-# my_list = rainfall_mi.split(",")
+my_list = rainfall_mi.split(",")
 
-# num_rainy_months = 0
+num_rainy_months = 0
 
-# print(my_list)
+print(my_list)
 
  
 
-# for num in my_list:
+for num in my_list:
 
-#     if float(num) > 3.0:
+    if float(num) > 3.0:
 
-#         num_rainy_months += 1
+        num_rainy_months += 1
 
-# print(num_rainy_months)
+print(num_rainy_months)
 
-# #The variable sentence stores a string. Write code to determine how many words in sentence start and end with the same letter, including one-letter words. Store the result in the variable same_letter_count.
+#The variable sentence stores a string. Write code to determine how many words in sentence start and end with the same letter, including one-letter words. Store the result in the variable same_letter_count.
 
-# #Hard-coded answers will receive no credit.
-
-
-# sentence = "students flock to the arb for a variety of outdoor activities such as jogging and picnicking"
-# 2
-# ​
-# 3
-# # Write your code here.
-# 4
-# words = sentence.split()
-# 5
-# same_letter_count = 0
-# 6
-# for word in words:
-# 7
-#     if word[0] == word[-1]:
-# 8
-#         same_letter_count += 1
-# 9
-# print(same_letter_count)   
-
-# #Write code to count the number of strings in list items that have the character w in it. Assign that number to the variable acc_num.
-# #HINT 1: Use the accumulation pattern!
-
-# #HINT 2: the in operator checks whether a substring is present in a string.
-
-# #Hard-coded answers will receive no credit.
+#Hard-coded answers will receive no credit.
 
 
-# items = ["whirring", "wow!", "calendar", "wry", "glass", "", "llama","tumultuous","owing"]
+sentence = "students flock to the arb for a variety of outdoor activities such as jogging and picnicking"
+2
+​
+3
+# Write your code here.
+4
+words = sentence.split()
+5
+same_letter_count = 0
+6
+for word in words:
+7
+    if word[0] == word[-1]:
+8
+        same_letter_count += 1
+9
+print(same_letter_count)   
 
-# acc_num = 0
+#Write code to count the number of strings in list items that have the character w in it. Assign that number to the variable acc_num.
+#HINT 1: Use the accumulation pattern!
 
-# for i in items:
+#HINT 2: the in operator checks whether a substring is present in a string.
 
-#     if "w" in i[0:]:
+#Hard-coded answers will receive no credit.
+
+
+items = ["whirring", "wow!", "calendar", "wry", "glass", "", "llama","tumultuous","owing"]
+
+acc_num = 0
+
+for i in items:
+
+    if "w" in i[0:]:
 
         
 
-#         acc_num += 1
+        acc_num += 1
 
-#     print(acc_num)
+    print(acc_num)
 
     s = "singing in the rain and playing in the rain are two entirely different situations but both can be fun"
 vowels = ['a','e','i','o','u']
